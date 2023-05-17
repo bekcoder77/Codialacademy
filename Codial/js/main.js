20966991840cae11e364d0ec27cce828c370bb46
bekcoder77 <127332753+bekcoder77@users.noreply.github.com>
2023-05-17 19:10:03 +0500

    Add files via upload

---
+++ Codial/js/main.js
@@ -0,0 +1,13 @@
+let  close = document.querySelector('.close');
+let  btn = document.querySelector('.btn');
+let  text = document.querySelectorAll('.footertext');
+
+btn.addEventListener('click',function(){
+    text.style.display ='block'
+})
+
+
+     
+   
+
+    
